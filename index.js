df32b4e8c9db663fd65242dc9cb404354e0e3b16
WoraponK <worapon.klabsri@gmail.com>
2023-12-20 18:25:18 +0700

    update redis

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 const express = require('express');
-const axios = require('axios');
+const uuid = require('uuid')
 const redis = require('redis');
-const PORT = 4000;
-
 const app = express();
+const client = redis.createClient();
 
-const redisPort = 6379;
-const client = redis.createClient(redisPort)
+// Connect to Redis
+client.connect().then(() => console.log('Connected to Redis')).catch(err => console.error('Redis connection error:', err));
 
-app.get("/", (req, res) => {
-    client.set("name", "poomwarawat", (err, reply) => {
-      res.send(reply);
-    });
-  });
+app.use(express.json())
+// API endpoints
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await client.hVals('todos');
+    const parsedTodos = todos.map(todo => JSON.parse(todo));
+    res.json(parsedTodos);
+  } catch (err) {
+    console.error('Error fetching todos:', err);
+    res.status(500).json({ error: 'Failed to retrieve todos' });
+  }
+});
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port`, PORT)
-})
+app.post('/todos', async (req, res) => {
+  try {
+    const { task_title, task_complete = false } = req.body; // Use default value for task_complete
+
+    if (!task_title) {
+      res.status(400).send({ error: 'Invalid todo data: task_title is required' });
+      return;
+    }
+
+    const todo_id = uuid.v4();
+
+    await client.hSet('todos', task_title, JSON.stringify({
+      id: todo_id,
+      task_title,
+      task_complete
+    }));
+
+    res.status(201).send({ message: 'Todo added successfully' });
+  } catch (err) {
+    console.error('Error adding todo:', err);
+    res.status(500).json({ error: 'Failed to add todo' });
+  }
+});
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
