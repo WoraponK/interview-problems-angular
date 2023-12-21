427b98ea2cec45a8bedbbb5f3f2127b8718e5e48
WoraponK <worapon.klabsri@gmail.com>
2023-12-21 22:01:21 +0700

    update works

--- src/app/components/medium-1/medium-1.component.ts
+++ src/app/components/medium-1/medium-1.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-medium-1',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgFor],
   templateUrl: './medium-1.component.html',
   styles: ``
 })
-export class Medium1Component {
+export class Medium1Component implements OnInit {
+  answers: any[] = [];
+
+  inputForm = this.fb.group({
+    answer: ['']
+  })
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    console.log(this.answers)
+  }
+
+  onSubmit() {
+    const answer = this.inputForm.value.answer;
+
+    if (!answer) {
+      alert('Please input number');
+      return;
+    }
+
+    const number = Number(answer);
+    const primeArray = [];
+
+    for (let i = 1; i <= number; i++) {
+      if (this.isPrime(i)) {
+        primeArray.push(i);
+      }
+    }
+
+    const primeArrayString = primeArray.join(" ");
+
+    this.answers.push(number + ' -> ' + primeArrayString)
+  }
+
+  isPrime(n: number): boolean {
+    if (n <= 1) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 
 }
