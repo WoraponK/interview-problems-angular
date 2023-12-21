2f72eee0a6eae2178131b9ebf8a35f6bdc6e2796
WoraponK <worapon.klabsri@gmail.com>
2023-12-21 17:02:52 +0700

    update works

--- src/app/components/easy-1/easy-1.component.ts
+++ src/app/components/easy-1/easy-1.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-easy-1',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './easy-1.component.html',
   styles: ``
 })
-export class Easy1Component {
+export class Easy1Component implements OnInit{
+  numbers: any[] = [];
 
+  constructor(){}
+
+  ngOnInit(): void {
+    for (let i = 1; i <= 100; i++) {
+      if ( i%3 == 0 && i%5 == 0 ) {
+        this.numbers.push("FizzBuzz");
+      } else if ( i%3 == 0 ) {
+        this.numbers.push("Fizz")
+      } else if ( i%5 == 0 ) {
+        this.numbers.push("Buzz")
+      } else {
+        this.numbers.push(i)
+      }
+    }
+  }
 }
